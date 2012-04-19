49a5d9f72f2309d51ee48f17c80aa838ef685f41
Mike Pacific <mike.pacific@gmail.com>
2012-04-18 19:57:13 -0700

    First commit

---
+++ Resources/ui/common/ApplicationTabGroup.js
@@ -0,0 +1,29 @@
+function ApplicationTabGroup(Window) {
+	//create module instance
+	var self = Ti.UI.createTabGroup();
+	
+	//create app tabs
+	var win1 = new Window('Schedule'),
+		win2 = new Window('Chants');
+	
+	var tab1 = Ti.UI.createTab({
+		title: 'Schedule',
+		icon: '/images/KS_nav_ui.png',
+		window: win1
+	});
+	win1.containingTab = tab1;
+	
+	var tab2 = Ti.UI.createTab({
+		title: 'Chants',
+		icon: '/images/KS_nav_views.png',
+		window: win2
+	});
+	win2.containingTab = tab2;
+	
+	self.addTab(tab1);
+	self.addTab(tab2);
+	
+	return self;
+};
+
+module.exports = ApplicationTabGroup;
